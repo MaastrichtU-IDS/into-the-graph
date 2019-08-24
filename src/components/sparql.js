273efb1c353c9cd2929b3efd6707f6cfb3b08ce4
vemonet <vincent.emonet@gmail.com>
2019-08-24 20:03:17 +0200

    Try to add YASGUI but everything freezes (maybe YASGUI is too heavy for react? It is working absolutely fine on Angular). change again the whole structure of the JS in Sparql because apparently it is more common (even if all material-ui examples are built using a different structure with weird function(), and even if some people use ts, and others jsx)

--- src/components/sparql.js
+++ src/components/sparql.js
@@ -1,27 +1,50 @@
-import React from "react";
-import { makeStyles } from '@material-ui/styles';
+import React, { Component } from "react";
+import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container';
- 
-const useStyles = makeStyles(theme => ({
+
+// Everything freezes when we import YASGUI.
+// Such a robust and reliable framework
+import '../assets/css/yasgui.min.css';
+import YASGUI from '../assets/js/yasgui.min.js'
+
+const styles = theme => ({
   menuButton: {
-    // background: '#fafafa'
     color: '#fafafa',
     marginRight: '1em',
     marginLeft: '1em',
     textTransform: 'none'
   }
-}));
- 
-export default function Sparql() {
-  const classes = useStyles();
+})
 
-  return (
-    <Container>
-      <Typography component="p">
-        This is the SPARQL me page.
-      </Typography >
-    </Container>
-  );
+class Sparql extends Component {
+  state = {}
+
+  componentDidMount() {
+    YASGUI.defaults.yasqe.sparql.endpoint = 'http://graphdb.dumontierlab.com/repositories/test';
+    // var config = {"api":{"urlShortener":"//yasgui.org/shorten"}};
+    const yasgui = YASGUI(document.getElementById('yasguiDiv'));
+    yasgui.addTab('statisticsTab');
+    yasgui.selectTab('statisticsTab').rename('Graphs statistics');
+    yasgui.selectTab('statisticsTab').setQuery(this.statisticsQuery);
+    yasgui.addTab('entitiesRelationsTab');
+    yasgui.selectTab('entitiesRelationsTab').rename('Explore entities relations');
+    yasgui.selectTab('entitiesRelationsTab').setQuery(this.entitiesRelationsQuery);
+  }
+
+  render () {
+    const { classes } = this.props;
+
+    return <Container>
+        <Typography component="p">
+          This is the SPARQL me page.
+          <div id="yasguiDiv"></div>
+        </Typography >
+      </Container>
+  }
 }
+
+export default withStyles(styles)(Sparql);
+
+
  
