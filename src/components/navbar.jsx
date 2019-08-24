f1ce2e832f33209ff224b072e2eca1389109eca9
vemonet <vincent.emonet@gmail.com>
2019-08-24 17:34:27 +0200

    switch from bootstrap to material UI

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
+import Typography from '@material-ui/core/Typography'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { makeStyles, mergeClasses } from '@material-ui/styles';
 
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import {AppBar, Toolbar, Button} from '@material-ui/core';
+
+// const useStyles = makeStyles(theme => ({
+//   flexGrow: {
+//     flexGrow: 1
+//   }
+// }));
  
 class NavBar extends Component {
+  // const classes = useStyles();
+  
   render() {
     return (
       <React.Fragment>
-        <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="/">rdf-graph-explorer</Navbar.Brand>
-            <Nav className="mr-auto">
-            <Link to="/">Home</Link>
-            <Link to="/sparql">SPARQL</Link>
-            <Link to="/describe">Describe</Link>
-            </Nav>
-        </Navbar>
+        <AppBar title="rdf-graph-explorer" position="static">
+          <Toolbar>
+            <Link to="/">
+              <Button color="inherit">
+                Home
+              </Button>
+            </Link>
+            <div className="flexGrow"></div>
+            <Link to="/describe">
+              <Button variant="h6">
+                Describe
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
       </React.Fragment>
     );
   }
