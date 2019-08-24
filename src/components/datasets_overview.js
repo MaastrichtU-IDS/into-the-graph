afabd8adfec4a2bbd6ada3acafe0b0e5f6d753a4
vemonet <vincent.emonet@gmail.com>
2019-08-24 21:57:02 +0200

    manage to display a dataTables test properly

--- src/components/datasets_overview.js
+++ src/components/datasets_overview.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react"; 
+import Container from '@material-ui/core/Container';
 // import $ from 'jquery'
 
 // import DataTable from 'datatables.net'
 // $.DataTable = DataTable
 
+// import DataTableDt from 'datatables.net-dt'
+
+import 'datatables.net-dt/css/jquery.dataTables.min.css'
 const $ = require('jquery');
 $.DataTable = require('datatables.net');
 
@@ -15,12 +19,15 @@ class Sparql extends Component {
   // state = {columns: [{title: 'Name', width: 120,data: 'name'},{title: 'Nickname',width: 180,data: 'nickname'}]}
 
   componentDidMount() {
-    $(this.refs.main).DataTable({
-       dom: '<"data-table-wrapper"t>',
-       data: this.props.names,
-       columns,
-       ordering: false
-    });
+    // This fails to get columns data,so the test data is in the render.
+    // $(this.refs.main).DataTable({
+    //    dom: '<"data-table-wrapper"t>',
+    //    data: this.props.names,
+    //    columns,
+    //    ordering: true
+    // });
+
+    $(this.refs.main).DataTable();
   }
 
   componentWillUnmount(){
@@ -36,9 +43,69 @@ class Sparql extends Component {
 
   render() {
       return (
-          <div>
-              <table ref="main" />
-          </div>);
+          <Container>
+              <table table ref="main" className="row-border">
+                <thead>
+                  <tr>
+                    <th>Dataset</th>
+                    <th>date generated</th>
+                    <th># of triples</th>
+                    <th># of entities</th>
+                    <th># of properties</th>
+                    <th># of classes</th>
+                    <th>Download as RDF/XML</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      Datas1
+                    </td>
+                    <td>
+                      date
+                    </td>
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      2  
+                    </td>
+                    <td>
+                      3
+                    </td>
+                    <td>
+                      4
+                    </td>
+                    <td>
+                      5
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Datas2
+                    </td>
+                    <td>
+                      date2
+                    </td>
+                    <td>
+                      2
+                    </td>
+                    <td>
+                      3
+                    </td>
+                    <td>
+                      4
+                    </td>
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      2
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+          </Container>);
   }
 }
 
