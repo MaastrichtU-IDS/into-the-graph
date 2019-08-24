78d37cd2a471ba862d934398343f849b385cf020
vemonet <vincent.emonet@gmail.com>
2019-08-24 23:04:57 +0200

    properly retrieve ?uri params in describe

--- src/components/describe.js
+++ src/components/describe.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { makeStyles } from '@material-ui/styles';
+import React, { Component } from "react";
+import { withStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container';
  
-const useStyles = makeStyles(theme => ({
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+ 
+const styles = theme => ({
   menuButton: {
-    // background: '#fafafa'
     color: '#fafafa',
     marginRight: '1em',
     marginLeft: '1em',
     textTransform: 'none'
   }
-}));
- 
-export default function Describe() {
-  const classes = useStyles();
+})
 
-  return (
-    <Container>
-      <Typography component="p">
-        This is the describe me page.
-      </Typography >
-    </Container>
-  );
+class Describe extends Component {
+  params = new URLSearchParams(location.search);
+
+  componentDidMount() {
+    console.log(this.params.get('uri'));
+  }
+  render () {
+    return <Container>
+        <Typography component="p">
+          This is the describe me page.
+          {this.params.get("uri")}
+        </Typography >
+      </Container>
+  }
 }
  
+export default withStyles(styles)(Describe);
