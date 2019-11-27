2a4488326f8194deb5bf11f934256df16f1e9813
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-27 12:58:57 +0100

    update prefixRegisgry to resolve URI namespace at display

--- src/components/link_describe.js
+++ src/components/link_describe.js
@@ -19,13 +19,13 @@ export function LinkDescribe(props) {
   const prefixRegistry = {
     bl: 'http://w3id.org/biolink/vocab/',
     biolink: 'https://w3id.org/biolink/vocab/',
-    d2s: 'https://w3id.org/data2services/',
+    d2s: 'https://w3id.org/d2s/',
     rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
     dc: 'http://purl.org/dc/elements/1.1/',
     obo: 'http://purl.obolibrary.org/obo/',
     owl: 'http://www.w3.org/2002/07/owl#',
-    ido: 'http://identifiers.org/',
+    ido: 'https://identifiers.org/',
   };
 
   function shortenUri(uriToShorten) {
