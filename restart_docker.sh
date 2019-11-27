ac11a8c54c4d09c0427748be35c8be290abaed54
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-27 19:06:30 +0100

    fix restart docker sh

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker stop linked-data-browser
-docker run --rm -it --name linked-data-browser -e VIRTUAL_HOST=ld-browser.137.120.31.101.nip.io linked-data-browser
+docker rm linked-data-browser
+docker run -d --name linked-data-browser -e VIRTUAL_HOST=ld-browser.137.120.31.101.nip.io linked-data-browser
