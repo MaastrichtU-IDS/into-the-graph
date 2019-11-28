714182da0a834b08293c6fc5c94bdcf66d2d74c2
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-28 08:25:50 +0100

    make navbar a component instead of function

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { makeStyles } from '@material-ui/styles';
+import React, { Component } from "react";
+import { withStyles } from '@material-ui/core/styles';
+import { Redirect, Link } from "react-router-dom";
 
 import {AppBar, Toolbar, Button} from '@material-ui/core';
 import Icon from '@material-ui/core/Icon';
@@ -10,58 +10,62 @@ import SearchBar from 'material-ui-search-bar'
 // import Typography from '@material-ui/core/Typography'
 
 // background: '#fafafa'
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   menuButton: {
     color: '#fafafa',
     marginRight: '1em',
     marginLeft: '1em',
     textTransform: 'none'
   }
-}));
+})
  
-export default function NavBar() {
-  const classes = useStyles();
-
-  return (
-    <React.Fragment>
-      <AppBar title="linked-data-browser" position="static">
-        <Toolbar>
-          <Link to="/">
-            <Button className={classes.menuButton}>
-            <Icon>explore</Icon>
-              &nbsp;Explore datasets
-            </Button>
-          </Link>
-          <Link to="/sparql">
-            <Button className={classes.menuButton}>
-              <Icon>share</Icon>
-              &nbsp;Run SPARQL queries
-            </Button>
-          </Link>
-          <Link to="/describe?uri=http:%2F%2Fidentifiers.org%2FHGNC:4600">
-            <Button className={classes.menuButton}>
-              {/* <Icon>search</Icon> */}
-              <Icon>format_list_bulleted</Icon>
-              &nbsp;Describe URIs
-            </Button>
-          </Link>
-          <div className="flexGrow"></div>
-          <SearchBar
-            onChange={() => console.log('onChange')}
-            onRequestSearch={() => console.log('onRequestSearch')}
-            style={{
-              margin: '0 auto',
-              maxWidth: 800
-            }}
-          />
-          <Button className={classes.menuButton} target="_blank"
-          href="https://github.com/MaastrichtU-IDS/linked-data-browser">
-            <Icon>code</Icon>
-            &nbsp;Source on GitHub
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-  );
+// export default function NavBar() {
+class NavBar extends Component {
+  // const classes = useStyles();
   
-}
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <AppBar title="linked-data-browser" position="static">
+          <Toolbar>
+            <Link to="/">
+              <Button className={classes.menuButton}>
+              <Icon>explore</Icon>
+                &nbsp;Explore datasets
+              </Button>
+            </Link>
+            <Link to="/sparql">
+              <Button className={classes.menuButton}>
+                <Icon>share</Icon>
+                &nbsp;Run SPARQL queries
+              </Button>
+            </Link>
+            <Link to="/describe?uri=http:%2F%2Fidentifiers.org%2FHGNC:4600">
+              <Button className={classes.menuButton}>
+                {/* <Icon>search</Icon> */}
+                <Icon>format_list_bulleted</Icon>
+                &nbsp;Describe URIs
+              </Button>
+            </Link>
+            <div className="flexGrow"></div>
+            <SearchBar
+              onChange={() => console.log('onChange')}
+              onRequestSearch={() => {return <Redirect to='/describe?uri=http:%2F%2Fidentifiers.org%2FHGNC:4601'/>}}
+              style={{
+                margin: '0 auto',
+                maxWidth: 800
+              }}
+            />
+            <Button className={classes.menuButton} target="_blank"
+            href="https://github.com/MaastrichtU-IDS/linked-data-browser">
+              <Icon>code</Icon>
+              &nbsp;Source on GitHub
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
+    );
+  }
+} export default withStyles(styles)(NavBar);
