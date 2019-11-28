3e7e1aa5073caedc255d5d2fc1ce1c53d211d4a1
Vincent Emonet <vincent.emonet@gmail.com>
2019-11-28 09:02:07 +0100

    add build to docker restart

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker build -t linked-data-browser .
 docker stop linked-data-browser
 docker rm linked-data-browser
 docker run -d --name linked-data-browser -e VIRTUAL_HOST=ld-browser.137.120.31.101.nip.io linked-data-browser
