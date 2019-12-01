720b9970c19c2bcc759bbbe9d86abff198a340af
Vincent Emonet <vincent.emonet@gmail.com>
2019-12-01 19:46:50 +0100

    change YASGUI display and default queries tabs

--- src/components/sparql.js
+++ src/components/sparql.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Paper from '@material-ui/core/Paper';
 import YASGUI from 'yasgui/dist/yasgui.bundled.min.js';
 import 'yasgui/dist/yasgui.min.css'
 
@@ -12,6 +11,9 @@ const styles = theme => ({
     marginRight: '1em',
     marginLeft: '1em',
     textTransform: 'none'
+  },
+  paperPadding: {
+    padding: theme.spacing(2, 2),
   }
 })
 
@@ -19,61 +21,60 @@ class Sparql extends Component {
   state = {}
 
   statisticsQuery = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX dct: <http://purl.org/dc/terms/>
-  PREFIX bl: <http://w3id.org/biolink/vocab/>
-  PREFIX dctypes: <http://purl.org/dc/dcmitype/>
-  PREFIX idot: <http://identifiers.org/idot/>
-  PREFIX dcat: <http://www.w3.org/ns/dcat#>
-  PREFIX void: <http://rdfs.org/ns/void#>
-  SELECT distinct ?source ?statements ?entities ?properties ?classes ?graph
-  WHERE {
-    GRAPH ?g {
-      ?dataset a dctypes:Dataset ; idot:preferredPrefix ?source .
-      ?version dct:isVersionOf ?dataset ; dcat:distribution ?rdfDistribution .
-      ?rdfDistribution a void:Dataset ;
-        dcat:accessURL ?graph ;
-        void:triples ?statements ;
-        void:entities ?entities ;
-        void:properties ?properties .
-      ?rdfDistribution void:classPartition [
-        void:class rdfs:Class ;
-        void:distinctSubjects ?classes
-      ] .
-    }
-  } ORDER BY DESC(?statements)`;
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX bl: <http://w3id.org/biolink/vocab/>
+PREFIX dctypes: <http://purl.org/dc/dcmitype/>
+PREFIX idot: <http://identifiers.org/idot/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX void: <http://rdfs.org/ns/void#>
+SELECT distinct ?source ?statements ?entities ?properties ?classes ?graph
+WHERE {
+  GRAPH ?g {
+    ?dataset a dctypes:Dataset ; idot:preferredPrefix ?source .
+    ?version dct:isVersionOf ?dataset ; dcat:distribution ?rdfDistribution .
+    ?rdfDistribution a void:Dataset ;
+      dcat:accessURL ?graph ;
+      void:triples ?statements ;
+      void:entities ?entities ;
+      void:properties ?properties .
+    ?rdfDistribution void:classPartition [
+      void:class rdfs:Class ;
+      void:distinctSubjects ?classes
+    ] .
+  }
+} ORDER BY DESC(?statements)`;
 
   entitiesRelationsQuery = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX dct: <http://purl.org/dc/terms/>
-  PREFIX dctypes: <http://purl.org/dc/dcmitype/>
-  PREFIX idot: <http://identifiers.org/idot/>
-  PREFIX dcat: <http://www.w3.org/ns/dcat#>
-  PREFIX void: <http://rdfs.org/ns/void#>
-  PREFIX void-ext: <http://ldf.fi/void-ext#>
-  SELECT distinct ?source ?classCount1 ?class1 ?relationWith ?classCount2 ?class2
-  WHERE {
-    GRAPH ?g {
-      ?dataset a dctypes:Dataset ; idot:preferredPrefix ?source .
-      ?version dct:isVersionOf ?dataset ; dcat:distribution ?rdfDistribution .
-      ?rdfDistribution a void:Dataset ;
-        dcat:accessURL ?graph .
-      ?rdfDistribution void:classPartition [
-        void:class rdfs:Class ;
-        void:distinctSubjects ?classes
-      ] .
-      ?rdfDistribution void:propertyPartition [
-          void:property ?relationWith ;
-          void:classPartition [
-              void:class ?class1 ;
-              void:distinctSubjects ?classCount1 ;
-          ];
-          void-ext:objectClassPartition [
-            void:class ?class2 ;
-            void:distinctObjects ?classCount2 ;
-      ]] .
-    }
-  } ORDER BY ?source DESC(?classCount1) DESC(?classCount2)`;
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dctypes: <http://purl.org/dc/dcmitype/>
+PREFIX idot: <http://identifiers.org/idot/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX void: <http://rdfs.org/ns/void#>
+PREFIX void-ext: <http://ldf.fi/void-ext#>
+SELECT distinct ?source ?classCount1 ?class1 ?relationWith ?classCount2 ?class2
+WHERE {
+  GRAPH ?g {
+    ?dataset a dctypes:Dataset ; idot:preferredPrefix ?source .
+    ?version dct:isVersionOf ?dataset ; dcat:distribution ?rdfDistribution .
+    ?rdfDistribution a void:Dataset ;
+      dcat:accessURL ?graph .
+    ?rdfDistribution void:classPartition [
+      void:class rdfs:Class ;
+      void:distinctSubjects ?classes
+    ] .
+    ?rdfDistribution void:propertyPartition [
+        void:property ?relationWith ;
+        void:classPartition [
+            void:class ?class1 ;
+            void:distinctSubjects ?classCount1 ;
+        ];
+        void-ext:objectClassPartition [
+          void:class ?class2 ;
+          void:distinctObjects ?classCount2 ;
+    ]] .
+  }
+} ORDER BY ?source DESC(?classCount1) DESC(?classCount2)`;
   
-
   componentDidMount() {
     YASGUI.defaults.yasqe.sparql.endpoint = 'http://graphdb.dumontierlab.com/repositories/test';
     // var config = {"api":{"urlShortener":"//yasgui.org/shorten"}};
@@ -87,13 +88,12 @@ class Sparql extends Component {
   }
 
   render () {
-    // const { classes } = this.props;
+    const { classes } = this.props;
     return <Container maxWidth="xl">
-        <Card className='mainContainer' style={{textAlign: 'left'}}>
-          <CardContent>
-            <div id="yasguiDiv"></div>
-          </CardContent>
-        </Card>
+        <Paper elevation={2} style={{textAlign: 'left'}}
+        className={['mainContainer', classes.paperPadding].join(' ')}>
+          <div id="yasguiDiv"></div>
+        </Paper>
       </Container>
   }
 }
