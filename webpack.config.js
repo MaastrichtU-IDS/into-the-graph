12af57faace041276b22866740a26f28afdefe4c
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-16 00:50:14 +0100

    resolve favicon issue by generating default expo webpack index.html and changing /public to /web

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,13 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 // var HtmlWebpackPlugin = require('html-webpack-plugin');
-// const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-// var CopyWebpackPlugin = require('copy-webpack-plugin');
-
-// See https://stackoverflow.com/questions/37298215/add-favicon-with-react-and-webpack
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize the config before returning it.
-  // config.plugins = [new HtmlWebpackPlugin({
-  //   favicon: "../../../../public/favicon.ico"
-  //   // template: [__dirname, 'public', 'favicon.ico'].join('/')
-  // })]
+  // Customize the config before returning it (example here)
   
-  // config.plugins = [
-  //   new FaviconsWebpackPlugin('../../../../public/favicon.ico')
-  // ]
-
-  // config.plugins = [new CopyWebpackPlugin([
-  //   // relative path is from src
-  //   { from: './public/favicon.ico' }, // <- your path to favicon
-  // ])]
+  // config.plugins = [new HtmlWebpackPlugin({
+  //   favicon: "./web/favicon.ico"
+  // })]
   return config;
 };
