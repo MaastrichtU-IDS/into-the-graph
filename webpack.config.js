01b854457cdd1350a705ad53a15d919b8a0662b2
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-18 02:04:18 +0100

    remove warning in asset size limit and remove useless dependencies and devDependencies (mostly leftovers from npm and react-script build)

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,11 @@ module.exports = async function(env, argv) {
   // config.plugins = [new HtmlWebpackPlugin({
   //   favicon: "./web/favicon.ico"
   // })]
+  // Don't display warning in asset size limit
+  config.performance = {
+    hints : false,
+    // maxEntrypointSize: 512000,
+    // maxAssetSize: 512000
+  };
   return config;
 };
