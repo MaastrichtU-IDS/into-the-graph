36e26b7bb90328cec2f20b6ba457d02ea72e4759
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-23 21:41:33 +0100

    fix getAllGrphsQuery to make it faster for virtuoso

--- src/components/GraphsOverview.js
+++ src/components/GraphsOverview.js
@@ -232,12 +232,7 @@ class GraphsOverview extends Component {
       </Container>);
   }
 
-  getAllGraphsQuery = `SELECT DISTINCT ?graph
-  WHERE {
-    GRAPH ?graph {
-      [] ?dummyProp [] .
-    }
-  }`
+  getAllGraphsQuery = `SELECT DISTINCT ?graph WHERE { GRAPH ?graph {?s ?p ?o} }`
 
   hclsOverviewQuery = `PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX dct: <http://purl.org/dc/terms/>
