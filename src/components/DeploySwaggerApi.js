d2e523dc735a47e9294326c46d7862b413e486c4
Vincent Emonet <vincent.emonet@gmail.com>
2020-02-07 10:05:54 +0100

    raise the height of the API iFrame to avoid cutting it (and double scroll)

--- src/components/DeploySwaggerApi.js
+++ src/components/DeploySwaggerApi.js
@@ -10,7 +10,7 @@ class DeploySwaggerApi extends Component {
     return <Iframe url={Config.swagger_api_url}
       width="100%"
       // TODO: properly fix height to avoid double scroll
-      height="180%"
+      height="900%"
       id="iFrameSwaggerApi"
       className="iFrameSwaggerApi"
       display="initial"
