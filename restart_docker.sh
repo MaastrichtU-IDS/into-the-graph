526d4cc2dbd203ff48c1a7bf0c98cf5c2b66863b
Vincent Emonet <vincent.emonet@gmail.com>
2020-02-15 21:45:01 +0100

    fix port in restart_docker.sh script

--- restart_docker.sh
+++ restart_docker.sh
@@ -3,4 +3,4 @@ git pull
 docker build -t into-the-graph .
 docker stop into-the-graph
 docker rm into-the-graph
-docker run -d --name into-the-graph -p 8082:80 -e VIRTUAL_HOST=into-bio2rdf.137.120.31.101.nip.io into-the-graph
+docker run -d --name into-the-graph -p 8082:5000 -e VIRTUAL_HOST=into-bio2rdf.137.120.31.101.nip.io into-the-graph
