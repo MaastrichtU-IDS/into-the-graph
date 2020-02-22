fafed53d880cf70792b185e404b3a67eac9d559a
Vincent Emonet <vincent.emonet@gmail.com>
2020-02-22 19:43:11 +0100

    now using default html iframe instead of react-iframe

--- src/components/DeployComunica.js
+++ src/components/DeployComunica.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
-import Iframe from 'react-iframe'
 
 var Config = require('Config')
 
 class DeployComunica extends Component {
   
   render () {
-    // const { classes } = this.props;
-    return <Iframe url={Config.comunica_url}
-      width="100%"
-      // TODO: properly fix height to avoid double scroll
-      height="180%"
-      id="iFrameComunica"
-      className="iFrameComunica"
-      display="initial"
-      position="relative"
-      scrolling="no" 
-      frameBorder="0"/>
+    return <iframe src={Config.comunica_url}
+        width="100%"
+        height='200%'
+        scrolling="no" 
+        frameBorder="0"
+      />
   }
 }
 export default (DeployComunica);
--- src/components/DeploySwaggerApi.js
+++ src/components/DeploySwaggerApi.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
-import Iframe from 'react-iframe'
+// import IframeResizer from 'react-iframe-resizer-super'
+// import IframeResizer from 'iframe-resizer-react'
+// import Iframe from 'react-iframe'
 
 var Config = require('Config')
 
 class DeploySwaggerApi extends Component {
-  
-  render () {
+
+  render() {
     // const { classes } = this.props;
-    return <Iframe url={Config.swagger_api_url}
-      width="100%"
-      // TODO: properly fix height to avoid double scroll
-      height="900%"
-      id="iFrameSwaggerApi"
-      className="iFrameSwaggerApi"
-      display="initial"
-      position="relative"
-      scrolling="no" 
-      frameBorder="0"/>
+    return <iframe 
+        src={Config.swagger_api_url}
+        // TODO: find ideal fix to resize iframe dynamically with content? 
+        // iFrameResizer didn't work
+        width='100%'
+        height='800%'
+        scrolling='no' 
+        frameBorder='0'
+      />;
   }
+
+  //   return <IframeResizer 
+  //      heightCalculationMethod="bodyScroll"
+  //      src={Config.swagger_api_url}
+  //     //  style={{ height: '100%', width:'100%' }}
+  //     //  iframeResizerOptions={{ checkOrigin: false }}
+  //     />
 }
 export default (DeploySwaggerApi);
