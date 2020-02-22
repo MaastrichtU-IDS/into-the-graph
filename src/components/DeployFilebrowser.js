0acfed48e30e180cbba64cdaf9d46a513c17e5fb
Vincent Emonet <vincent.emonet@gmail.com>
2020-02-22 19:53:58 +0100

    fix filebrowser iframe (stop using react-iframe) and set download as href instead of link to in navbar

--- src/components/DeployFilebrowser.js
+++ src/components/DeployFilebrowser.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Iframe from 'react-iframe'
 
 var Config = require('Config')
 
@@ -7,18 +6,12 @@ class DeployFilebrowser extends Component {
   
   render () {
     // const { classes } = this.props;
-    // return <table className={'iFrameTable'}>
-    return <Iframe url={Config.download_filebrowser_url}
+    return <iframe url={Config.download_filebrowser_url}
         width="100%"
         // TODO: properly fix height to avoid double scroll
-        height="100%"
-        id="iFrameFilebrowser"
-        className={'iFrameStyle'}
-        display="initial"
-        position="relative"
+        height="800%"
         scrolling="no" 
         frameBorder="0"/>
-    // </table>
   }
 }
 export default (DeployFilebrowser);
