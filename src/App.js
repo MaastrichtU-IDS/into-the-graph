e5693ad13aee6d9da7ba668c61eeae192eee980e
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-18 16:37:43 +0100

    change font to 13px

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const theme = createMuiTheme({
   },
   typography: {
     "fontFamily": "\"Open Sans\", \"Roboto\", \"Arial\"",
-    "fontSize": 12,
+    "fontSize": 13,
     "fontWeightLight": 300,
     "fontWeightRegular": 400,
     "fontWeightMedium": 500
