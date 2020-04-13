91849d4da9383d9474023397715d2c6db7643daf
Vincent Emonet <vincent.emonet@gmail.com>
2020-04-13 10:51:47 +0200

    fix restart_docker.sh script

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-docker build -t into-the-graph .
+docker build -t umids/into-the-graph .
 docker stop into-the-graph
-docker rm into-the-graph
-docker run -d --name into-the-graph -p 8082:5000 -e VIRTUAL_HOST=into-bio2rdf.137.120.31.101.nip.io into-the-graph
+
+docker run -d --rm --name into-the-graph -p 8082:5000 -e VIRTUAL_HOST=into-the-graph.137.120.31.102.nip.io umids/into-the-graph:latest
